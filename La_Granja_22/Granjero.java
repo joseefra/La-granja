14ebde5d7e690e72edd0175acbccda84a00374e1
joseefra <vert2jose@gmail.com>
2016-11-14 19:10:07 -0600

    camina granjero

--- La_Granja_22/Granjero.java
+++ La_Granja_22/Granjero.java
@@ -1,5 +1,5 @@
 import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
-
+import java.awt.image.*;
 /**
  * Write a description of class Granjero here.
  * 
@@ -7,7 +7,23 @@ import greenfoot.*;  // (World, Actor, GreenfootImage, Greenfoot and MouseInfo)
  * @version (a version number or a date)
  */
 public class Granjero extends Actor
-{
+  { 
+private GreenfootImage image1= new GreenfootImage("jugador3.png");//dere
+private GreenfootImage image2= new GreenfootImage("jugador2.png");//Izqui 
+private GreenfootImage image3= new GreenfootImage("jugador1.png");
+private GreenfootImage image4= new GreenfootImage("jugador4.png");
+
+private GreenfootImage image5= new GreenfootImage("jugador7.png");//derecha
+private GreenfootImage image6= new GreenfootImage("jugador6.png");//izquierda   
+private GreenfootImage image7= new GreenfootImage("jugador8.png");//arriba
+private GreenfootImage image8= new GreenfootImage("jugador5.png");//abajo
+
+
+   public int direccion;
+   private int activado = 0;
+   private int speed = 5 ;
+   public int i;
+
     /**
      * Act - do whatever the Granjero wants to do. This method is called whenever
      * the 'Act' or 'Run' button gets pressed in the environment.
@@ -15,7 +31,10 @@ public class Granjero extends Actor
     public void act() 
     {
         // Add your action code here.
+        if(activado == 0)
+        {
         movimientos();
+         }
     }
     public void  movimientos()
     {
@@ -23,30 +42,48 @@ public class Granjero extends Actor
         if(Greenfoot.isKeyDown("right"))
         {
             
-            setRotation(0);
-            move(3);
+           direccion = 1;                                  //1 Derecha
+            this.setLocation(this.getX()+speed,this.getY());
+            setImage(image1);
+            i++;
+            if(i%2 == 0)
+               setImage(image5);
+            
            
         }
-      //  Disparar(); 
+     
         //mueve a el granjero para la izquierda
         if(Greenfoot.isKeyDown("left"))
         {
-           setRotation(-0);
-            move(-3);
-           
+          direccion = 2;                                  //2 Izquierda
+            this.setLocation(this.getX()-speed,this.getY());
+            setImage(image2);
+            i++;
+            if(i%2 == 0)
+               setImage(image6);
+             
         }
          //mover para arriba a el granjero del usuario
         if(Greenfoot.isKeyDown("up"))
         {
-            setRotation(-90);
-            move(3);
+            direccion = 3;                                 
+             this.setLocation(this.getX(),this.getY()-speed);
+            setImage(image4);
+            i++;
+            if(i%2 == 0)
+               setImage(image7);
            
         }
          //mover el granjero del usuario a hacia abajo      
         if(Greenfoot.isKeyDown("down"))
         {
-          setRotation(90);
-            move(3);  
+         direccion = 4;                                 
+            this.setLocation(this.getX(),this.getY()+speed);
+            setImage(image3);
+            i++;
+            if(i%2 == 0)
+               setImage(image8);
+              
         }  
     }
 }
--- La_Granja_22/Nivel1.java
+++ La_Granja_22/Nivel1.java
@@ -17,12 +17,13 @@ public class Nivel1 extends World
     {    
         // Create a new world with 600x400 cells with a cell size of 1x1 pixels.
         super(900, 600, 1);
-        preparate();
+        inicia(); 
     }
-    void preparate()
+    void inicia()
     {
-        Granjero j1= new Granjero();
-        addObject(j1,200,200);
+    Granjero j1 = new Granjero();
+    addObject(j1,200,200);
     }
+    
     
 }
